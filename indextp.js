6c5c0e05c42a83c2ecc00d903a2d1ca5b03acb62
BrunoMont-M <142555496+BrunoMont-M@users.noreply.github.com>
2023-12-18 10:41:03 -0300

    Arreglando urlo

--- indextp.js
+++ indextp.js
@@ -153,7 +153,7 @@ function crearComprador() {
             total_precio: document.getElementById("total_precio").innerHTML
         };
     
-        fetch(`https://brunomont-m.github.io/CaC23544-Front/web-app-tpfinal-23544/api/compradores`, {
+        fetch(`https://brunomont-m.github.io/web-app-tpfinal-23544/api/compradores`, {
             method: "POST",
             body: JSON.stringify(comprador),
         })

2a3cfe5741c34e1e1331cc8bac7e912ca3fa7197
BrunoMont-M <142555496+BrunoMont-M@users.noreply.github.com>
2023-12-18 10:32:35 -0300

    Corrigiendo url para conectar a github pages

--- indextp.js
+++ indextp.js
@@ -153,7 +153,7 @@ function crearComprador() {
             total_precio: document.getElementById("total_precio").innerHTML
         };
     
-        fetch(`http://localhost:8080/web-app-tpfinal-23544/api/compradores`, {
+        fetch(`https://brunomont-m.github.io/CaC23544-Front/web-app-tpfinal-23544/api/compradores`, {
             method: "POST",
             body: JSON.stringify(comprador),
         })
